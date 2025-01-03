fa3a9a141f1fd09db406b35ecfdcf397c71a5972
techingneer-2003 <coderbescy@gmail.com>
2025-01-03 14:37:02 +0530

    For loop creation

---
+++ For-Loop.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int a=5,b=10;
+    for(a;a<10;a++){
+             for(b;b<15;b++){
+            
+            printf("World!\t");
+        }
+        
+        printf("\nHello\n");
+
+   
+        }
+        
+        
+
+} 
